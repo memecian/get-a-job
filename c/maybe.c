37e11f46e22656e3abff34243fefd9ea91246e16
Alexander "memecian" (ufhkt) <ememecian@gmail.com>
2023-01-15 01:51:58 +0100

    added additional exposition

--- c/maybe.c
+++ c/maybe.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "maybe.h"
-#include <pthread.h>
+#include <unistd.h>
 #include <SDL2/SDL_mixer.h>
 
 #define white_fg "\033[37m"
 #define blue_fg "\033[34m"
 #define black_bg "\033[40m"
 #define bold "\033[1m"
+#define italics "\033[3m"
 #define reset_txt "\033[0m\033[40m"
 #define no_fmt "\033[0m"
 
@@ -29,8 +30,22 @@ int main(void) {
     }
     // fprintf(stderr, "Script loaded.\n");
 
-    printf("%s%s", white_fg, black_bg);
+    // init audio
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        printf("Error initializing SDL_mixer: %s\n", Mix_GetError());
+        return 1;
+    }
 
+    Mix_Music *music = Mix_LoadMUS("conversation.mp3");
+    if (!music) {
+        printf("Error loading music: %s\n", Mix_GetError());
+        return 1;
+    }
+
+    Mix_PlayMusic(music, 1);
+    printf("%sYou enter a bathroom in the UNATCO HQ. The door closes behind you.%s\n", italics, reset_txt);
+    printf("%s%s", white_fg, black_bg);
+    sleep(3);
     int i = 0;
     // fprintf(stderr, "Starting main loop.\n");
     do {
@@ -56,12 +71,15 @@ int main(void) {
         //fprintf(stderr, "Message read.\n");
 
         talk(name_buffer, message_buffer);
+        sleep(2);
 end:
         free(name_buffer);
         free(message_buffer);
     } while (char_buf != EOF);
 
     // we're done here
+    Mix_FadeOutMusic(2000);
+    Mix_CloseAudio();
     fclose(script);
     printf("%s", no_fmt);
     return EXIT_SUCCESS;
@@ -75,16 +93,5 @@ int move(int direction, int distance) {
 int talk(char* name, char* message) {;
     printf("%s%s%s", bold, white_fg, name);
     printf("%s%s%s\n", reset_txt , blue_fg, message);
-    return 0;
-}
-
-int play_music(char* filename) {
-    
-    
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
-        printf("Error initializing SDL_mixer: %s\n", Mix_GetError());
-        return 1;
-    }
-
     return 0;
 }
